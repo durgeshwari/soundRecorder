d4edfad74b4f57bfc5178fe0888a5b0b4977dd20
durgeshwari <durgeshwarinaik@gmail.com>
2019-05-19 14:17:55 +0530

    my second commit

--- app/src/main/java/com/example/recorder/TabFragment2.java
+++ app/src/main/java/com/example/recorder/TabFragment2.java
@@ -2,14 +2,87 @@ package com.example.recorder;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class TabFragment2 extends Fragment {
 
+
+    private Toolbar toolbar;
+    private RecyclerView recyclerViewRecordings;
+    private ArrayList<Recording> recordingArraylist;
+    private RecordingAdapter recordingAdapter;
+    private TextView textViewNoRecordings;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tab2, container, false);
+        View rview= inflater.inflate(R.layout.activity_recording_list, container, false);
+
+
+        recordingArraylist = new ArrayList<Recording>();
+
+        /** setting up recyclerView **/
+        recyclerViewRecordings = (RecyclerView) rview.findViewById(R.id.recyclerViewRecordings);
+        recyclerViewRecordings.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerViewRecordings.setAdapter(recordingAdapter);
+        Log.d("debugMode", "The onCreateView method has been launched");
+//        recyclerViewRecordings.setHasFixedSize(true);
+
+        textViewNoRecordings = (TextView) rview.findViewById(R.id.textViewNoRecordings);
+
+
+        fetchRecordings();
+
+
+        return rview;
+
+    }
+
+    
+    private void fetchRecordings() {
+
+        File root = android.os.Environment.getExternalStorageDirectory();
+        String path = root.getAbsolutePath() + "/VoiceRecorderSimplifiedCoding/Audios";
+        Log.d("Files", "Path: " + path);
+        File directory = new File(path);
+        File[] files = directory.listFiles();
+        Log.d("Files", "Size: "+ files.length);
+        if( files!=null ){
+
+            for (int i = 0; i < files.length; i++) {
+
+                Log.d("Files", "FileName:" + files[i].getName());
+                String fileName = files[i].getName();
+                String recordingUri = root.getAbsolutePath() + "/VoiceRecorderSimplifiedCoding/Audios/" + fileName;
+
+                Recording recording = new Recording(recordingUri,fileName,false);
+                recordingArraylist.add(recording);
+            }
+
+            textViewNoRecordings.setVisibility(View.GONE);
+            recyclerViewRecordings.setVisibility(View.VISIBLE);
+            setAdaptertoRecyclerView();
+
+        }else{
+            textViewNoRecordings.setVisibility(View.VISIBLE);
+            recyclerViewRecordings.setVisibility(View.GONE);
+        }
+
+    }
+
+    private void setAdaptertoRecyclerView() {
+        recordingAdapter = new RecordingAdapter(getActivity(),recordingArraylist);
+        recyclerViewRecordings.setAdapter(recordingAdapter);
     }
 }
